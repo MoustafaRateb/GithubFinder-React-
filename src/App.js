2653e7ca4a83f4d749bf4b5e359887d1c5217af8
MoustafaRateb <moustafa.m.rateb@gmail.com>
2021-10-09 16:08:36 +0200

    addPagebtns

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Search from './components/users/Search';
 import Users from './components/users/Users';
+import parse from 'parse-link-header'
 
 class App extends Component {
 
   state = {
+    nextUrl :'',
+    prevUrl :'',
     users: [],
     loading: false
   }
@@ -15,20 +18,63 @@ class App extends Component {
     this.setState({loading : true});
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
     client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    let linkData = parse(res.headers.link);
+    this.setState({nextUrl:linkData['next']?linkData['next'].url:'',
+    prevUrl:linkData['prev']?linkData['prev']:''});
     this.setState({users : res.data});
     this.setState({loading : false});
-    
   }
+  
     searchUsers =async text =>{
-      this.setState({loading : true});
-      const res = await axios.get(`https://api.github.com/search/users?q=${text}`);
-      this.setState({users : res.data.items});
-      this.setState({loading : false});
+      if (text !== ''){
+
+        this.setState({loading : true});
+        const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+        client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&q=${text}`);
+        this.setState({users : res.data.items});
+        this.setState({loading : false});
+      }
+    }
+
+    nextPage =async () =>{
+      
+      console.log(this.state.nextUrl);
+      if (this.state.nextUrl !== ''){
+
+        this.setState({loading : true});
+        const err = await axios.get('https://httpbin.org/status/404').
+  catch(err => err);
+          console.log(err);
+          const resPromise =  axios.get(this.state.nextUrl).catch(function (error) {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
+          });
+          // const res = await resPromise;
+          // console.log(res);
+          // this.setState({users : res.data});
+          // this.setState({loading : false});
+        
+        
+      }
     }
     render(){
       return (
       <div className="App">
-        <Navbar />
+        <Navbar nextPage = {this.nextPage}/>
         <div className="container">
           <Search searchUsers = {this.searchUsers} />
           <Users loading={this.state.loading} users= {this.state.users}/>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PropTypes  from 'prop-types'
-const Navbar = ({icon,title}) => {
+const Navbar = ({icon,title,nextPage}) => {
         return (
             <nav className="navbar bg-primary">
                 <h1>
                     <i className={icon}></i> {title}
                 </h1>
                 <form className="text-center">
-                    <button   className="btn btn-sm btn-toolbar btn-success">
-                    <i class="fas fa-arrow-alt-circle-left"></i>
+                    <button   className="btn  btn-toolbar btn-primary" onClick = {nextPage}>
+                    <i className="fas fa-arrow-alt-circle-left"></i>
                     </button>
-                    <button className="btn btn-sm btn-toolbar btn-success">
-                    <i class="fas fa-arrow-alt-circle-right"></i>
+                    <button className="btn  btn-toolbar btn-primary" onClick = {nextPage}>
+                    <i className="fas fa-arrow-alt-circle-right"></i>
                     </button>
                 </form>
             </nav>
@@ -19,11 +19,13 @@ const Navbar = ({icon,title}) => {
 }
 Navbar.defaultProps = {
     title:"Github Finder",
-    icon:"fab fa-github"
+    icon:"fab fa-github",
+    nextPage:()=>{}
   }
 
   Navbar.propTypes = {
       title:PropTypes.string.isRequired,
-      icon:PropTypes.string.isRequired
+      icon:PropTypes.string.isRequired,
+      nextPage:PropTypes.func.isRequired
   }
 export default Navbar
